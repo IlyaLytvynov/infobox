f97786d7a4672709f50904507c408e57d2b89852
Illia Lytvynov <ilyaillych@gmail.com>
2019-11-25 14:01:03 +0200

    Updates

---
+++ precache-manifest.318052c192f31b66442450e3a221be59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6736d0a1b52c0767270b7aab8a4d64",
+    "url": "/infobox/index.html"
+  },
+  {
+    "revision": "54a91c31d9b61340a52c",
+    "url": "/infobox/static/css/main.13a4e686.chunk.css"
+  },
+  {
+    "revision": "ffae56da8268b9a52c3e",
+    "url": "/infobox/static/js/2.a91d9574.chunk.js"
+  },
+  {
+    "revision": "54a91c31d9b61340a52c",
+    "url": "/infobox/static/js/main.b3e5c590.chunk.js"
+  },
+  {
+    "revision": "7ddcf51b8c0d2f74b1f1",
+    "url": "/infobox/static/js/runtime-main.e23a6bb3.js"
+  }
+]);
